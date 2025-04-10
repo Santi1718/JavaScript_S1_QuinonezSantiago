e29fe42f3a912bc1d88257ea6b7fb9507516bce2
Santi1718 <sayara.arciniegas@gmail.com>
2025-04-10 07:33:46 -0500

    feat: :zap: My first commit

---
+++ Dia1.1/tarea.js
@@ -0,0 +1,19 @@
+console.log("----Bienvenidos a Campuslands----")
+console.log("1. Trainer")
+console.log("2. Coordinador")
+console.log("3. Camper")
+console.log("4. Salir del panel de inicio")
+
+alert("----Bienvenidos a Campuslands----")
+let opcion = prompt ("Selecciona tu rol:\n 1. Trainer:\n 2. Coordinador:\n 3. Camper:\n 4. Salir del panel de inicio ")
+
+if (opcion == 1) {
+    alert("has iniciado como Trainer")
+    Trainer = true
+    let opcion = prompt("1. Ingresa tu correo\n 2. Ingresa tu contraseña")
+    inicio = true
+
+
+
+
+}
---
+++ Dia1/script.js
@@ -0,0 +1,22 @@
+//Mis primeros pasos en JavaScript
+console.log("Hola")
+//Datos primitivos - Numeros Enteros
+numerito1 = 1;
+numerito2 = 2,14;
+console.log(typeof numerito1);
+console.log(typeof numerito2);
+numerito3 = numerito1 + numerito2;
+//concatenación general
+console.log(numerito3+" "+typeof numerito3);
+//Operaciones Generales (PEMDAS)
+operaciones=(((((5*2)**2)+256)/5)*2);
+console.log(operacion);
+operacion2 = operacion%2;
+console.log(operacion2);
+//Booleanos - Operadores Lógicos
+Booleano1= true;
+Booleano2=false; 
+Booleano3=false;
+Booleano4= true;
+operadoresBandera = (booleano1 && booleano2);
+console.log(operadoresBandera);
